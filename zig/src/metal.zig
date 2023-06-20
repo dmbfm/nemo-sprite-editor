3fa850a7e856f7553ba2c54f0eef7483980c795c
Daniel Fortes <daniel.mbfm@gmail.com>
2023-06-19 23:28:51 -0300

    Bindings

--- zig/src/metal.zig
+++ zig/src/metal.zig
@@ -1,2 +1,15 @@
-pub const Device = @import("metal/device.zig").Device;
-pub const CommandQueue = @import("metal/command_queue.zig").CommandQueue;
+pub usingnamespace @import("metal/buffer.zig");
+pub usingnamespace @import("metal/command_buffer.zig");
+pub usingnamespace @import("metal/command_queue.zig");
+pub usingnamespace @import("metal/device.zig");
+pub usingnamespace @import("metal/drawable.zig");
+pub usingnamespace @import("metal/enums.zig");
+pub usingnamespace @import("metal/function.zig");
+pub usingnamespace @import("metal/library.zig");
+pub usingnamespace @import("metal/render_command_encoder.zig");
+pub usingnamespace @import("metal/render_pass_descriptor.zig");
+pub usingnamespace @import("metal/render_pipeline_descriptor.zig");
+pub usingnamespace @import("metal/render_pipeline_state.zig");
+pub usingnamespace @import("metal/structs.zig");
+pub usingnamespace @import("metal/texture.zig");
+pub usingnamespace @import("metal/texture_descriptor.zig");
